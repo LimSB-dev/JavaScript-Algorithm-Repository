39c0e13bf96e60271d7bd1785a06a3c206504e92
임성빈 <95527804+LimSB-dev@users.noreply.github.com>
2023-01-31 00:25:24 +0900

    ⚡️ Linked List JavaScript

---
+++ Linked-List/index.js
@@ -0,0 +1,33 @@
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
+
+class LinkedList {
+  constructor() {
+    this.head = null;
+    this.tail = null;
+    this.length = 0;
+  }
+
+  addToTail(value) {
+    const newNode = new Node(value);
+    if (!this.head) {
+      this.head = newNode;
+    } else {
+      this.tail.next = newNode;
+    }
+    this.tail = newNode;
+    this.length++;
+  }
+
+  removeFromHead() {
+    if (!this.head) return null;
+    const value = this.head.value;
+    this.head = this.head.next;
+    this.length--;
+    return value;
+  }
+}

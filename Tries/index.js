e7858aa08ce00b9869478b1184224cb7080150db
임성빈 <95527804+LimSB-dev@users.noreply.github.com>
2023-01-31 00:53:02 +0900

    ⚡️ Tries JavaScript

---
+++ Tries/index.js
@@ -0,0 +1,30 @@
+class Trie {
+  constructor() {
+    this.root = new TrieNode();
+  }
+
+  insert(word) {
+    let node = this.root;
+    for (let char of word) {
+      if (!node.children[char]) node.children[char] = new TrieNode();
+      node = node.children[char];
+    }
+    node.isEnd = true;
+  }
+
+  search(word) {
+    let node = this.root;
+    for (let char of word) {
+      if (!node.children[char]) return false;
+      node = node.children[char];
+    }
+    return node.isEnd;
+  }
+}
+
+class TrieNode {
+  constructor() {
+    this.children = {};
+    this.isEnd = false;
+  }
+}

e716c0df0cac4b087d2fbeef7f1e5a9c81f582bb
임성빈 <95527804+LimSB-dev@users.noreply.github.com>
2023-01-31 00:41:39 +0900

    ⚡️ Queues JavaScript

---
+++ Queues/index.js
@@ -0,0 +1,60 @@
+// 시간 복잡도 O(n)
+```
+class Queue {
+  constructor() {
+    this.items = [];
+  }
+
+  enqueue(element) {
+    this.items.push(element);
+  }
+
+  dequeue() {
+    return this.items.shift();
+  }
+
+  peek() {
+    return this.items[0];
+  }
+
+  isEmpty() {
+    return this.items.length === 0;
+  }
+}
+```;
+
+// 시간 복잡도 O(1)
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
+
+class Queue {
+  constructor() {
+    this.front = null;
+    this.rear = null;
+    this.size = 0;
+  }
+
+  enqueue(value) {
+    const newNode = new Node(value);
+    if (!this.front) {
+      this.front = newNode;
+      this.rear = newNode;
+    } else {
+      this.rear.next = newNode;
+      this.rear = newNode;
+    }
+    this.size++;
+  }
+
+  dequeue() {
+    if (!this.front) return null;
+    const removedNode = this.front;
+    this.front = this.front.next;
+    this.size--;
+    return removedNode.value;
+  }
+}
